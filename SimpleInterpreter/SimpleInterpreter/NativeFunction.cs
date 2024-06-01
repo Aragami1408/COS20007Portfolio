6585b5c4e37ca4ee5d3b5b2cb0a9ead533d766d9
Aragami1408 <vucaominh1408@gmail.com>
2024-06-01 22:28:59 +1000

    Created native function wrapper class

---
+++ SimpleInterpreter/SimpleInterpreter/NativeFunction.cs
@@ -0,0 +1,24 @@
+﻿namespace SimpleInterpreter;
+
+public class NativeFunction : ICallable
+{
+  public NativeFunction(string name, Func<Interpreter, IReadOnlyList<object>, object> action, int arity = 0) {
+    Name = name;
+    Arity = arity;
+    this.action = action;
+  }
+
+  public string Name {get;}
+  private readonly Func<Interpreter, IReadOnlyList<object>, object> action;
+  public int Arity { get; }
+
+  public object call(Interpreter interpreter, params object[] arguments)
+  {
+    return action(interpreter, arguments);
+  }
+
+  public override string ToString()
+  {
+    return $"<native fn>";
+  }
+}
